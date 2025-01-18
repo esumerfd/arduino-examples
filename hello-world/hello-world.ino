cce1c921e21239c6cd363ddd1b9d6bb8cc6ef0b2
Ed Sumerfield <esumerfd@bitbashers.org>
2025-01-18 16:22:47 -0500

    finish hello world to prove serialization

--- hello-world/hello-world.ino
+++ hello-world/hello-world.ino
@@ -10,10 +10,11 @@
  *  stop_bits=1
  */
 void setup() {
-  Serial.begin(9600, SERIAL_8N1);
+  Serial.begin(9600);
   delay(100);
 }
 
 void loop() {
   Serial.println("hello world");
+  delay(1000);
 }
