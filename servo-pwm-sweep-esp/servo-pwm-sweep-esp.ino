aae246dfadfb51c509f928d3135507a072e10b98
Ed Sumerfield <esumerfd@bitbashers.org>
2025-12-10 19:58:06 -0500

    Update examples

--- servo-pwm-sweep-esp/servo-pwm-sweep-esp.ino
+++ servo-pwm-sweep-esp/servo-pwm-sweep-esp.ino
@@ -6,7 +6,7 @@
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
 #define SERVOMIN 150
-#define SERVOMAX 600
+#define SERVOMAX 545
 
 uint8_t servonum = 0;
 
@@ -14,10 +14,18 @@ void setup() {
   Log::begin();
   Flash::begin();
 
+  // ESP32CAM I2C pins (matching working robot-spider code)
+  Log::println("Initializing I2C on SDA=15, SCL=14");
+  Wire.begin(15, 14);
+
+  // Initialize PWM driver (matching Adafruit example and working code)
   pwm.begin();
-  pwm.setPWMFreq(60);
+  pwm.setOscillatorFrequency(27000000);  // Calibrate oscillator - CRITICAL!
+  pwm.setPWMFreq(50);  // 50 Hz for servos
+
   delay(10);
 
+  Log::println("PWM driver initialized");
   Flash::flash();
 }
 
@@ -39,4 +47,5 @@ void loop() {
   Flash::flash();
 
   Log::println("Done");
+  delay(1000);
 }
